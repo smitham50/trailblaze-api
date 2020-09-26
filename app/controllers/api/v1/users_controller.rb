0c3d08105d4f4186a8a8fac410bc8f66a4bd73f4
Aaron Smith <smitham50@gmail.com>
2020-09-26 15:33:33 -0400

    fixed indentation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,10 +18,10 @@ class Api::V1::UsersController < ApplicationController
     private
     def user_params
         params.require(:user).permit(
-        :username, 
-        :password,
-        :password_confirmation,
-        :email
+            :username, 
+            :password,
+            :password_confirmation,
+            :email
         )
     end
 end
