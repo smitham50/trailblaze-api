c4efd88f35c8ff0fc0b8d99a1f9dd50234cab6b4
Aaron Smith <smitham50@gmail.com>
2020-09-26 14:57:00 -0400

    added signup route, simple token, and auth_token to user schema

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::UsersController < ApplicationController
+    def create
+        byebug
+        @user = User.new(user_params)
+        if @user.save
+            login!
+            render json: {
+                status: :created,
+                user: @user.as_json(only: [:authentication_token])
+            }
+        else 
+            render json: {
+                status: 500,
+                errors: @user.errors.full_messages
+            }
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(
+        :username, 
+        :password,
+        :password_confirmation,
+        :email
+        )
+    end
+end
