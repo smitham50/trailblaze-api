cd47ec5b646eb0230155578cfaa0f38f180d3941
Aaron Smith <smitham50@gmail.com>
2020-12-14 09:40:06 -0500

    fix/moved skip_before_actions to trail and user_trail

--- app/controllers/api/v1/user_trails_controller.rb
+++ app/controllers/api/v1/user_trails_controller.rb
@@ -1,3 +1,3 @@
 class Api::V1::UserTrailsController < ApplicationController
-
+    skip_before_action :verify_authenticity_token
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::API
     include ActionController::RequestForgeryProtection
 
     protect_from_forgery with: :null_session
-    skip_before_action :verify_authenticity_token
     before_action :set_csrf_cookie
 
     def login!
