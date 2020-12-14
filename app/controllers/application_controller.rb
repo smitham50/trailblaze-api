c62db63fd4d1a94b85f003d07474308b924dbbcd
Aaron Smith <smitham50@gmail.com>
2020-12-14 09:34:04 -0500

    fix/added skip_before_action to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::API
     include ActionController::RequestForgeryProtection
 
     protect_from_forgery with: :null_session
+    skip_before_action :verify_authenticity_token
     before_action :set_csrf_cookie
 
     def login!
