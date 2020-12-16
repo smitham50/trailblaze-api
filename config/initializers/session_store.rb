eb9836ccea10413ae796e8110789fe312876d144
Aaron Smith <smitham50@gmail.com>
2020-12-16 10:15:19 -0500

    fix/csrf issue in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+# config/initializers/session_store.rb
+
 Rails.application.config.session_store :cookie_store, key: '_station_session'
 
-ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
+Rails.application.config.session_store(:active_record_store)
+
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
+end
