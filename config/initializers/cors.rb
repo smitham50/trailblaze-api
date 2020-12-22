7d85a0c5b732192699242da96713a8b77482e8b7
Aaron Smith <smitham50@gmail.com>
2020-12-22 09:18:02 -0500

    temporarily whitelisting localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://smitham50.github.io'
+    origins 'https://smitham50.github.io', 'localhost:3000'
 
     resource '*',
       headers: :any,
