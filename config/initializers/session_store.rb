11da290fae213a15f4f6e557fc1f8e02184e19ca
Aaron Smith <smitham50@gmail.com>
2020-12-29 22:08:09 -0500

    fix/session issue in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,2 @@
-Rails.application.config.session_store :cookie_store, key: '_station_session', secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_station_session'
+Rails.application.config.action_controller.forgery_protection_origin_check = false
